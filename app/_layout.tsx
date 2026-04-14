89041ca2d5accdcd1dcb918a6f39f75efca8e381
estebanfrm <giraldomontenegro2304@gmail.com>
2026-04-13 21:53:33 -0500

    feat: pantalla de listado de productos (comprador)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,6 +27,7 @@ const RootLayout = () => {
         <Stack.Screen name="index" />
         <Stack.Screen name="auth/login/index" />
         <Stack.Screen name="auth/register/index" />
+        <Stack.Screen name="products/index" />
         <Stack.Screen name="profile/index" />
       </Stack>
     </QueryClientProvider>
